d1445d5506e18b9bd268ba54d31a33ab3bbc9ebb
josh543 <joshthomas543@gmail.com>
2020-07-09 17:09:35 +0100

    Cleanup & now only shows shoes in stock

--- multi_page_scraper.py
+++ multi_page_scraper.py
@@ -1,5 +1,3 @@
-#import bs4 not sure if this is needed
-
 """
     script finds number of pages on webpage then extracts all climbing shoe names and prices and prints them to csv file.
 """
@@ -8,87 +6,48 @@ from bs4 import BeautifulSoup as soup
 import re
 
 #set vars
-price_limit = 70
-min_size = 8 #minimum shoe size?
-page = 1 #what page do you want to start from?
-
+PriceLimit = 65 #set price limit
+Size = 9 #set your shoe size
+ShoeSizes = {'7' : '2753' , '8': '2757' , '9': '2759', '10': '2762', '11': '2765', '12': '3851'}
 
 filename = "climbingshoes.csv"
 f = open(filename, "w")
-headers = "Name, Price, Link \n"
+headers = "Name, RRP (£), Price (£), Link \n"
 f.write(headers)
+#website is filtered for men and unisex
+website  = 'https://shop.epictv.co.uk/en/category/climbing-shoes?f%5B0%5D=field_gender%3A497&f%5B1%5D=field_gender%3A496&f%5B2%5D=field_product%253Afield_shoe_size%3A' + ShoeSizes.get(str(Size)) + '&page='
 
-website  = 'https://shop.epictv.co.uk/en/sale/outlet/climbing-shoes-outlet'
-uClient = uReq(website) #essentially opens up url and downloads info
+
+uClient = uReq(website) #opens up url and downloads info
 page_html = uClient.read() #saves source code as this var
 uClient.close()
 page_soup = soup(page_html, "html.parser")
 find_pages = page_soup.findAll("li", {"class":"pager-item desktop"})
 page_total = len(find_pages) + 1
-while page < page_total:
-    my_url = 'https://shop.epictv.co.uk/en/category/climbing-shoes?page=' + str(page)
+for page in range(0, page_total):
 
-    uClient = uReq(my_url) #essentially opens up url and downloads info
+    my_url = website + str(page)
+
+    uClient = uReq(my_url) #opens up url and downloads info
     page_html = uClient.read()
     uClient.close()
-
-    #now need to parse html as it is currently a big jumble of text
-
-    page_soup = soup(page_html, "html.parser") #telling it how to parse the file, here as an html source file
+    page_soup = soup(page_html, "html.parser")
     products = page_soup.findAll("div", {"class":"product-item-row clearfix"})
-
-    #gets link
-    link = products[0].findAll("div", {"class":"field-name-title-field"})
-    full_link = 'https://shop.epictv.co.uk' + str(link[0].a.attrs['href'])
-    print(link[0].a.attrs['href'])
 
 
     ##########
-    filename = "climbingshoes.csv"
-    f = open(filename, "a")
 
     for idx, product in enumerate(products): #idx gives location (from 0)
-    #    print('count is', idx+1)
+        _price = product.findAll("span", {"class":"price-value"})
+        price = _price[0].text.replace('£','')
+        if float(price) < PriceLimit:
+            _name = product.findAll("div", {"class":"field-name-title-field"})
+            _rrp = product.findAll("div", {"class":"price-strikethrough"})
+            name = _name[0].text.strip()
+            full_link = 'https://shop.epictv.co.uk' + str(_name[0].a.attrs['href'])
+            rrp = _rrp[0].text.strip().replace('£','')
 
-        product_name = product.findAll("div", {"class":"field-name-title-field"})
-        name = product_name[0].text.strip()
-        full_link = 'https://shop.epictv.co.uk' + str(product_name[0].a.attrs['href'])
-
-        product_price = product.findAll("span", {"class":"price-value"})
-        price = product_price[0].text.replace('£','')
-        x = float(price)
-        if x < price_limit:
-
-            uClient = uReq(full_link)
-            page2_html = uClient.read()
-            page2_soup = soup(page2_html, "html.parser")
-            sizes = page2_soup.findAll("div", {"class":"size-link-wrapper"})
-            if len(sizes)>0:
-                Lsize = re.findall("\d+", sizes[-1].text)
-    #            print(Lsize)
-                if float(Lsize[0]) > min_size:
-                    Fsize = re.findall("\d+", sizes[0].text)
-    #                print('First:', Fsize, 'Last:', Lsize)
-                    if float(Fsize[0]) > float(Lsize[0]) or float(Lsize[0])>13:
-    #                    print(name, 'number', idx+1, 'must be kids shoe')
-                        None
-                    else:
-                        f.write(name + "," + price + "," + full_link  + "\n")
-
-
-
-
-
-            #sizes come in order so just end the loop if contains +<10
-
-
-
-#sizes[20].text
-        #print("Shoe: " + name)
-        #print("Price: " + price)
-
-
-#        print (int(price))
+            f = open(filename, "a")
+            f.write(name + "," + rrp + "," + price + "," + full_link  + "\n")
 
     f.close()
-    page += 1
